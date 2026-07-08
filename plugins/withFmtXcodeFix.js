6133888752056a96cdab1b967149719794b49cce
Aaron Pilk <aaron@skyway.media>
2026-07-08 19:29:37 -0400

    Fix RN codegen script phase for paths with spaces (quote WITH_ENVIRONMENT/SCRIPT_PHASES_SCRIPT)

--- plugins/withFmtXcodeFix.js
+++ plugins/withFmtXcodeFix.js
@@ -12,6 +12,17 @@ const PATCH_MARKER = 'PRC patch: force consteval off';
 
 const PATCH = `
     # Fix fmt consteval build errors on newer Xcode/Clang (RN 0.76 ships fmt 9.x).
+    installer.pods_project.targets.each do |target|
+      target.build_phases.each do |phase|
+        next unless phase.respond_to?(:shell_script) && phase.shell_script
+        broken = '/bin/sh -c "$WITH_ENVIRONMENT $SCRIPT_PHASES_SCRIPT"'
+        fixed = '/bin/sh -c ". \\"$WITH_ENVIRONMENT\\" && /bin/bash \\"$SCRIPT_PHASES_SCRIPT\\""'
+        if phase.shell_script.include?(broken)
+          phase.shell_script = phase.shell_script.sub(broken, fixed)
+        end
+      end
+    end
+
     fmt_base = File.join(installer.sandbox.root, 'fmt/include/fmt/base.h')
     if File.exist?(fmt_base)
       original = File.read(fmt_base)
