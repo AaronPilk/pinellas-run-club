8aede269d7e821c1a993ebc7cc6109a17648f264
Aaron Pilk <aaron@skyway.media>
2026-07-08 19:18:25 -0400

    Apply FMT_CONSTEVAL fix to all pod targets (fmt headers compiled by multiple React pods)

--- plugins/withFmtXcodeFix.js
+++ plugins/withFmtXcodeFix.js
@@ -13,12 +13,12 @@ const PATCH_MARKER = 'FMT_CONSTEVAL=';
 const PATCH = `
     # Fix fmt consteval build errors on newer Xcode/Clang (RN 0.76 ships fmt 9.x).
     installer.pods_project.targets.each do |target|
-      if target.name == 'fmt' || target.name == 'RCT-Folly'
-        target.build_configurations.each do |config|
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_CONSTEVAL='
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_USE_NONTYPE_TEMPLATE_ARGS=0'
-        end
+      target.build_configurations.each do |config|
+        defs = Array(config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] || ['$(inherited)'])
+        defs << '$(inherited)' unless defs.include?('$(inherited)')
+        defs << 'FMT_CONSTEVAL=' unless defs.include?('FMT_CONSTEVAL=')
+        defs << 'FMT_USE_NONTYPE_TEMPLATE_ARGS=0' unless defs.include?('FMT_USE_NONTYPE_TEMPLATE_ARGS=0')
+        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] = defs
       end
     end
 `;
